c3cd08f65a20a504c2354b8f487638ddae8b4be3
Chuyen Trung Tran <chuyentt@gmail.com>
2020-08-12 09:28:11 +0700

    Completed AcDbPoint data model

--- lib/src/datamodels/ac_db_point.dart
+++ lib/src/datamodels/ac_db_point.dart
@@ -12,20 +12,93 @@ class AcDbPoint implements AcDbEntity {
     if (result != null) handle = int.tryParse(result.value, radix: 16);
     var _acDbEntity = AcDbPoint._init(handle);
     _acDbEntity._groupCodes.addAll(codes);
+
+    result =
+        codes.firstWhere((element) => element.code == 10, orElse: () => null);
+    if (result != null) {
+      _acDbEntity._x = double.parse(result.value);
+    }
+
+    result =
+        codes.firstWhere((element) => element.code == 20, orElse: () => null);
+    if (result != null) {
+      _acDbEntity._y = double.parse(result.value);
+    }
+
+    result =
+        codes.firstWhere((element) => element.code == 30, orElse: () => null);
+    if (result != null) {
+      _acDbEntity._z = double.parse(result.value);
+    }
+
+    result =
+        codes.firstWhere((element) => element.code == 8, orElse: () => null);
+    if (result != null) {
+      _acDbEntity._layerName = result.value;
+    }
+
     return _acDbEntity;
   }
 
   AcDbPoint(this._handle,
-      {double x = 0, double y = 0, double z = 0, String layer = '0'}) {
+      {double x = 0, double y = 0, double z = 0, String layerName = '0'})
+      : _x = x,
+        _y = y,
+        _z = z,
+        _layerName = layerName {
     _groupCodes.add(GroupCode(0, 'POINT'));
     _groupCodes.add(GroupCode(5, handle.toRadixString(16)));
     _groupCodes.add(GroupCode(330, '1F'));
     _groupCodes.add(GroupCode(100, 'AcDbEntity'));
-    _groupCodes.add(GroupCode(8, layer));
+    _groupCodes.add(GroupCode(8, layerName));
     _groupCodes.add(GroupCode(100, 'AcDbPoint'));
     _groupCodes.add(GroupCode(10, x));
     _groupCodes.add(GroupCode(20, y));
     _groupCodes.add(GroupCode(30, z));
+  }
+
+  double _x = 0;
+  double get x => _x;
+  set x(double value) {
+    var result = _groupCodes.firstWhere((element) => element.code == 10,
+        orElse: () => null);
+    if (result != null) {
+      _x = value;
+      result.value = value;
+    }
+  }
+
+  double _y = 0;
+  double get y => _y;
+  set y(double value) {
+    var result = _groupCodes.firstWhere((element) => element.code == 20,
+        orElse: () => null);
+    if (result != null) {
+      _y = value;
+      result.value = value;
+    }
+  }
+
+  double _z = 0;
+  double get z => _z;
+  set z(double value) {
+    var result = _groupCodes.firstWhere((element) => element.code == 30,
+        orElse: () => null);
+    if (result != null) {
+      _z = value;
+      result.value = value;
+    }
+  }
+
+  String _layerName = '0';
+  String get layerName => _layerName;
+  set layerName(String value) {
+    var result = _groupCodes.firstWhere((element) => element.code == 8,
+        orElse: () => null);
+    if (result != null) {
+      _layerName = value;
+      result.value = value;
+    }
   }
 
   @override
