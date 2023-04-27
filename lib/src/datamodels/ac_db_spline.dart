7d59c0f3b5c572c41cff128d6afcf14c2eda6b44
Thobias Silva <thobias.silva@growdev.com.br>
2023-04-27 15:40:34 -0300

    rename prop vertices to controlPoints

--- lib/src/datamodels/ac_db_spline.dart
+++ lib/src/datamodels/ac_db_spline.dart
@@ -53,10 +53,10 @@ class AcDbSpline implements AcDbEntity {
 
     codes.forEach((element) {
       if (element.code == 10) {
-        _acDbEntity._vertices.add([double.parse(element.value)]);
+        _acDbEntity._controlPoints.add([double.parse(element.value)]);
         _acDbEntity._groupCodes.remove(element);
       } else if (element.code == 20) {
-        _acDbEntity._vertices.last.add(double.parse(element.value));
+        _acDbEntity._controlPoints.last.add(double.parse(element.value));
         _acDbEntity._groupCodes.remove(element);
       }
     });
@@ -64,10 +64,10 @@ class AcDbSpline implements AcDbEntity {
   }
 
   AcDbSpline({
-    required List<List<double>> vertices,
+    required List<List<double>> controlPoints,
     bool isClosed = false,
     String layerName = '0',
-  })  : _vertices = vertices,
+  })  : _controlPoints = controlPoints,
         _isClosed = isClosed,
         _layerName = layerName {
     _groupCodes.add(GroupCode(0, type));
@@ -76,21 +76,21 @@ class AcDbSpline implements AcDbEntity {
     _groupCodes.add(GroupCode(100, 'AcDbEntity'));
     _groupCodes.add(GroupCode(8, layerName));
     _groupCodes.add(GroupCode(100, 'AcDbSpline'));
-    _groupCodes.add(GroupCode(73, vertices.length));
+    _groupCodes.add(GroupCode(73, controlPoints.length));
     _groupCodes.add(GroupCode(70, isClosed ? 1 : 8));
     _groupCodes.add(GroupCode(43, 0.0));
-    _vertices.forEach((element) {
+    _controlPoints.forEach((element) {
       _groupCodes.add(GroupCode(10, element[0]));
       _groupCodes.add(GroupCode(20, element[1]));
     });
   }
 
-  var _vertices = <List<double>>[];
-  List<List<double>> get vertices => _vertices;
-  set vertices(List<List<double>> value) {
+  var _controlPoints = <List<double>>[];
+  List<List<double>> get controlPoints => _controlPoints;
+  set controlPoints(List<List<double>> value) {
     final result = _groupCodes.firstWhere((element) => element.code == 73);
     result.value = value.length;
-    _vertices = value;
+    _controlPoints = value;
   }
 
   bool _isClosed = false;
